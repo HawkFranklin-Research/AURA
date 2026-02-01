f9512e55703768daa6c0cb8d4a007c52b3b4a199
VatsalPatel18 <vatsal1804@gmail.com>
2026-02-01 20:48:39 +0530

    Bump version to 1.0.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
     applicationId = "com.hawkfranklin.aura"
     minSdk = 31
     targetSdk = 35
-    versionCode = 18
-    versionName = "1.0.2-beta"
+    versionCode = 19
+    versionName = "1.0.2"
 
     // Needed for HuggingFace auth workflows.
     // Use the scheme of the "Redirect URLs" in HuggingFace app.
